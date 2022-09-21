ed70996a71288df8833e5ac4fb8d9313e0e9b740
Oluwatayo <oluwatayorotimi@gmail.com>
2022-09-21 09:56:42 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates the string pointed to
+ * the end of the tring pointed to by @dest
+ * @dest: String to be appended
+ * @src: String to be concatenated
+ *
+ * Return: returns pointer to @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index ++);
+
+	return (dest);
+}
